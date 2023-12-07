dd33442db2d62ca23bb0b07d5c33d72ad36659b9
Ameer <ameerapex496@gmail.com>
2023-12-07 11:21:08 +0100

    added comments selected elements and added event linsters

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("hallo world");
+// selected elements
+var goodCheck = document.getElementsByClassName("good-Check")[0];
+var cheapCheck = document.getElementsByClassName("cheap-Check")[0];
+var fastCheck = document.getElementsByClassName("fast-Check")[0];
+// gelobal varibales
+// functions
+//event linsters
+goodCheck.addEventListener("click", goodHandler);
+cheapCheck.addEventListener("click", cheapHandler);
+fastCheck.addEventListener("click", fastHandler);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,13 @@
-console.log("hallo world");
+// selected elements
+const goodCheck = document.getElementsByClassName("good-Check")[0];
+const cheapCheck = document.getElementsByClassName("cheap-Check")[0];
+const fastCheck = document.getElementsByClassName("fast-Check")[0];
+
+// gelobal varibales
+
+// functions
+
+//event linsters
+goodCheck.addEventListener("click", goodHandler);
+cheapCheck.addEventListener("click", cheapHandler);
+fastCheck.addEventListener("click", fastHandler);
