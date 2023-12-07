32dce9ffb638e4a89473bdf4b566d1daf01c2bb6
Ameer <ameerapex496@gmail.com>
2023-12-07 11:32:06 +0100

    added function to the project

--- src/script.js
+++ src/script.js
@@ -2,8 +2,25 @@
 var goodCheck = document.getElementsByClassName("good-Check")[0];
 var cheapCheck = document.getElementsByClassName("cheap-Check")[0];
 var fastCheck = document.getElementsByClassName("fast-Check")[0];
-// gelobal varibales
 // functions
+var goodHandler = function () {
+    console.log("good-Clicked");
+    if (cheapCheck.checked) {
+        fastCheck.checked = false;
+    }
+};
+var cheapHandler = function () {
+    console.log("cheap-Clicked");
+    if (fastCheck.checked) {
+        goodCheck.checked = false;
+    }
+};
+var fastHandler = function () {
+    console.log("fast-Clicked");
+    if (goodCheck.checked) {
+        cheapCheck.checked = false;
+    }
+};
 //event linsters
 goodCheck.addEventListener("click", goodHandler);
 cheapCheck.addEventListener("click", cheapHandler);
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,29 @@
 // selected elements
-const goodCheck = document.getElementsByClassName("good-Check")[0];
-const cheapCheck = document.getElementsByClassName("cheap-Check")[0];
-const fastCheck = document.getElementsByClassName("fast-Check")[0];
-
-// gelobal varibales
+const goodCheck: any = document.getElementsByClassName("good-Check")[0];
+const cheapCheck: any = document.getElementsByClassName("cheap-Check")[0];
+const fastCheck: any = document.getElementsByClassName("fast-Check")[0];
 
 // functions
+const goodHandler = () => {
+  console.log("good-Clicked");
+  if (cheapCheck.checked) {
+    fastCheck.checked = false;
+  }
+};
+
+const cheapHandler = () => {
+  console.log("cheap-Clicked");
+  if (fastCheck.checked) {
+    goodCheck.checked = false;
+  }
+};
+
+const fastHandler = () => {
+  console.log("fast-Clicked");
+  if (goodCheck.checked) {
+    cheapCheck.checked = false;
+  }
+};
 
 //event linsters
 goodCheck.addEventListener("click", goodHandler);
